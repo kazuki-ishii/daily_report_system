aead6d87164bf9dc531e38bf0322555c38f0285b
kazuki.ishii <dream.0427.i.k@gmail.com>
2018-06-20 21:13:59 +0900

    最新

--- WebContent/WEB-INF/views/reports/show.jsp
+++ WebContent/WEB-INF/views/reports/show.jsp
@@ -27,7 +27,7 @@
                   </tr>
                   <tr>
                      <th>更新日時</th>
-                     <td><fmt:formatDate value="${report.update_at }" pattern="yyyy-MM-dd HH:mm:ss"/></td>
+                     <td><fmt:formatDate value="${report.updated_at }" pattern="yyyy-MM-dd HH:mm:ss"/></td>
                   </tr>
                </tbody>
             </table>
--- src/controllers/reports/ReportsUpdateServlet.java
+++ src/controllers/reports/ReportsUpdateServlet.java
@@ -37,32 +37,34 @@ public class ReportsUpdateServlet extends HttpServlet {
 	 */
 	protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
 		String _token = (String)request.getParameter("_token");
-		if(_token != null && _token.equals(request.getSession().getId())){
-			EntityManager em = DBUtil.createEntityManager();
+        if(_token != null && _token.equals(request.getSession().getId())) {
+            EntityManager em = DBUtil.createEntityManager();
 
-			Report r = em.find(Report.class,(Integer)(request.getSession().getAttribute("report_id")));
+            Report r = em.find(Report.class, (Integer)(request.getSession().getAttribute("report_id")));
 
-			r.setReport_date(Date.valueOf(request.getParameter("report.date")));
-			r.setTitle(request.getParameter("title"));
-			r.setContent(request.getParameter("content"));
-			r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
-			List<String> errors = ReportValidator.validate(r);
-			if(errors.size() > 0){
-				em.close();
+            r.setReport_date(Date.valueOf(request.getParameter("report_date")));
+            r.setTitle(request.getParameter("title"));
+            r.setContent(request.getParameter("content"));
+            r.setUpdated_at(new Timestamp(System.currentTimeMillis()));
 
-				request.setAttribute("_token", request.getSession().getId());
-				request.setAttribute("report", r);
-				request.setAttribute("errors", errors);
+            List<String> errors = ReportValidator.validate(r);
+            if(errors.size() > 0) {
+                em.close();
 
-				RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
-				rd.forward(request, response);
-			}else{
-				em.getTransaction().begin();
-				em.getTransaction().commit();
-				em.close();
-				request.getSession().setAttribute("flush", "更新が完了しました。");
-				request.getSession().removeAttribute("report_id");
-				response.sendRedirect(request.getContextPath() + "/reports/index");
+                request.setAttribute("_token", request.getSession().getId());
+                request.setAttribute("report", r);
+                request.setAttribute("errors", errors);
+
+                RequestDispatcher rd = request.getRequestDispatcher("/WEB-INF/views/reports/edit.jsp");
+                rd.forward(request, response);
+            } else {
+                em.getTransaction().begin();
+                em.getTransaction().commit();
+                em.close();
+                request.getSession().setAttribute("flush", "更新が完了しました。");
+
+                request.getSession().removeAttribute("report_id");
+
 			}
 		}
 	}
